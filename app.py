9857cda2f5b2e4f5d61856c1578ec9ef0c0d2bc6
Amith Ramaswamy <82469264+amith-2001@users.noreply.github.com>
2024-07-14 13:42:21 -0400

    Update app.py

--- app.py
+++ app.py
@@ -6,8 +6,8 @@ app = Flask(__name__)
 
 @app.route('/fetch')
 def hello_world():
-    with tempfile.NamedTemporaryFile(delete=False) as temp_file:
-        temp_file.write(b'Hello, World!')
+   # with tempfile.NamedTemporaryFile(delete=False) as temp_file:
+       #temp_file.write(b'Hello, World!')
     return "hello world"
 
 if __name__ == '__main__':
