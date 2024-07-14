9498fcaaf161c21aafae5cd51d0feb787e762bca
Amith Ramaswamy <82469264+amith-2001@users.noreply.github.com>
2024-07-14 13:40:19 -0400

    Update app.py

--- app.py
+++ app.py
@@ -8,12 +8,7 @@ app = Flask(__name__)
 def hello_world():
     with tempfile.NamedTemporaryFile(delete=False) as temp_file:
         temp_file.write(b'Hello, World!')
-
-    # Get the temporary file path
-    temp_file_path = temp_file.name
-
-    # print(f'Temporary file path: {temp_file_path}')
-    return temp_file_path
+    return "hello world"
 
 if __name__ == '__main__':
     app.run(debug=True)
